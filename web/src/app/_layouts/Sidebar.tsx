1095c68f0133851836b41283d4ea89b8034ec37a
andrewww05 <krvnd05@gmail.com>
2025-08-30 20:45:07 +0300

    Localization

--- web/src/app/_layouts/Sidebar.tsx
+++ web/src/app/_layouts/Sidebar.tsx
@@ -4,23 +4,25 @@ import { FC } from 'react';
 import Image from 'next/image';
 import clsx from 'clsx';
 import { usePathname } from 'next/navigation';
+import { useTranslations } from 'next-intl';
 
 const Sidebar: FC = () => {
     const pathname = usePathname()
+    const t = useTranslations("common");
     const menuItems = [
         {
             svg: (<path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />),
-            name: "Profile",
+            name: t('profile'),
             path: "/profile"
         },
         {
             svg: (<path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />),
-            name: "Home",
+            name: t('home'),
             path: "/"
         },
         {
             svg: (<path strokeLinecap="round" strokeLinejoin="round" d="M8.625 9.75a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 0 1 .778-.332 48.294 48.294 0 0 0 5.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />),
-            name: "Messages",
+            name: t('messages'),
             path: "/messages"
         },
         {
@@ -30,7 +32,7 @@ const Sidebar: FC = () => {
                     <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                 </>
             ),
-            name: "Settings",
+            name: t('settings'),
             path: "/settings"
         },
     ];
@@ -66,7 +68,7 @@ const Sidebar: FC = () => {
                                     </div>
 
                                     <div>
-                                        <span className="flex items-center items-center rounded-full bg-primary-bg size-7 inset-ring inset-ring-primary-border-hover">
+                                        <span className="flex items-center rounded-full bg-primary-bg size-7 inset-ring inset-ring-primary-border-hover">
                                             <span className="w-full text-sm font-medium text-primary-text text-center">5</span>
                                         </span>
                                     </div>
